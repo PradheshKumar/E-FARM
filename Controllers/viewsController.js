73db0c24a698d8027d5ae5237aab0148982753c5
PradheshKumar <pradheshkumar29@gmail.com>
2022-07-20 13:17:40 +0530

    Pug Changes

--- Controllers/viewsController.js
+++ Controllers/viewsController.js
@@ -1,6 +1,6 @@
-const Product = require('../models/productModel');
-const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
+const Product = require("../models/productModel");
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 exports.getIndex = catchAsync(async (req, res, next) => {
   // 1) Get product data from collection
@@ -8,8 +8,8 @@ exports.getIndex = catchAsync(async (req, res, next) => {
 
   // 2) Build template
   // 3) Render that template using product data from 1)
-  res.status(200).render('index', {
-    title: 'E-FARM',
+  res.status(200).render("index", {
+    title: "E-FARM",
     products,
   });
 });
@@ -19,25 +19,26 @@ exports.getOverview = catchAsync(async (req, res, next) => {
 
   // 2) Build template
   // 3) Render that template using product data from 1)
-  res.status(200).render('overview', {
-    title: 'E-FARM',
+  res.status(200).render("overview", {
+    title: "E-FARM",
     products,
   });
 });
+
 exports.getProduct = catchAsync(async (req, res, next) => {
   const product = await Product.findById(req.params.id).populate({
-    path: 'seller',
-    fields: 'name email photo',
+    path: "seller",
+    fields: "name email photo",
   });
   if (!product)
-    return next(new AppError('There is no product with that id', 404));
-  res.status(200).render('product', {
+    return next(new AppError("There is no product with that id", 404));
+  res.status(200).render("product", {
     title: `${product.name} product`,
     product,
   });
 });
 exports.getLoginForm = (req, res) => {
-  res.status(200).render('login', {
-    title: 'Log into your account',
+  res.status(200).render("login", {
+    title: "Log into your account",
   });
 };
